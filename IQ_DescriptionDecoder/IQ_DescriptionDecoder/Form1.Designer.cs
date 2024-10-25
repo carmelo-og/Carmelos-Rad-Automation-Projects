a718f7d0c90279d6e9a7ff85ef2198fb09676a57
carmelo-og <51840169+carmelo-og@users.noreply.github.com>
2024-10-25 04:55:27 -0700

    Add files via upload

---
+++ IQ_DescriptionDecoder/IQ_DescriptionDecoder/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace IQ_DescriptionDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkSpaceTags = new System.Windows.Forms.CheckBox();
+            this.btnHOA = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAlarms = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IQ_DescriptionDecoder.Properties.Resources.WonderVerter;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 224);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSource
+            // 
+            this.btnSource.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSource.Location = new System.Drawing.Point(98, 248);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(182, 53);
+            this.btnSource.TabIndex = 1;
+            this.btnSource.Text = "Source L5X";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveAs.Location = new System.Drawing.Point(98, 1117);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(182, 53);
+            this.btnSaveAs.TabIndex = 2;
+            this.btnSaveAs.Text = "Save As";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(98, 1201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 95);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(296, 1133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Path:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(1025, -2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(782, 864);
+            this.listBox1.TabIndex = 6;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(98, 522);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(379, 441);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReadFile.Location = new System.Drawing.Point(98, 365);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(182, 110);
+            this.btnReadFile.TabIndex = 8;
+            this.btnReadFile.Text = "Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 490);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Data Types:";
+            // 
+            // chkSpaceTags
+            // 
+            this.chkSpaceTags.AutoSize = true;
+            this.chkSpaceTags.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkSpaceTags.Location = new System.Drawing.Point(98, 988);
+            this.chkSpaceTags.Name = "chkSpaceTags";
+            this.chkSpaceTags.Size = new System.Drawing.Size(236, 33);
+            this.chkSpaceTags.TabIndex = 10;
+            this.chkSpaceTags.Text = "Add spaces to .tag";
+            this.chkSpaceTags.UseVisualStyleBackColor = true;
+            // 
+            // btnHOA
+            // 
+            this.btnHOA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHOA.Location = new System.Drawing.Point(360, 1205);
+            this.btnHOA.Name = "btnHOA";
+            this.btnHOA.Size = new System.Drawing.Size(189, 90);
+            this.btnHOA.TabIndex = 11;
+            this.btnHOA.Text = "Create HOA";
+            this.btnHOA.UseVisualStyleBackColor = true;
+            this.btnHOA.Click += new System.EventHandler(this.btnHOA_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(457, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(438, 1133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // btnAlarms
+            // 
+            this.btnAlarms.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlarms.Location = new System.Drawing.Point(597, 1201);
+            this.btnAlarms.Name = "btnAlarms";
+            this.btnAlarms.Size = new System.Drawing.Size(189, 90);
+            this.btnAlarms.TabIndex = 14;
+            this.btnAlarms.Text = "Create Alarms";
+            this.btnAlarms.UseVisualStyleBackColor = true;
+            this.btnAlarms.Click += new System.EventHandler(this.btnAlarms_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(301, 314);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(665, 35);
+            this.textBox2.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(98, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 52);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "CSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(1025, 888);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(782, 424);
+            this.listBox2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1808, 1324);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAlarms);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnHOA);
+            this.Controls.Add(this.chkSpaceTags);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkSpaceTags;
+        private System.Windows.Forms.Button btnHOA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAlarms;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ IQ_DescriptionDecoder/IQ_DescriptionDecoder/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Resources;
+using System.Reflection;
+
+namespace IQ_DescriptionDecoder
+{
+    public partial class Form1 : Form
+    {
+
+        XmlDocument doc = new XmlDocument();
+        // make collection of tags to add tags found into
+        Collection<IQ_X_CONTROL> Array_IQ_X_CONTROL = new Collection<IQ_X_CONTROL>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // instantiate XmlDocument and load XML from file
+
+        }
+
+        string pathFile, Outfolder, pathCSV;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                pathFile = file.FileName;
+                label4.Text = pathFile;
+            }
+        }
+
+        public class IQ_X_CONTROL
+        {
+            public string Name { get; set; }
+            public string DescPLC { get; set; }
+            public string DataType { get; set; }
+
+            public IQ_X_CONTROL() { }
+            public IQ_X_CONTROL(string nameIn, string descPLCIn, string dataTypeIn)
+            {
+                Name = nameIn;
+                DescPLC = descPLCIn;
+                DataType = dataTypeIn;
+            }
+        }
+
+        public string cdataWrap(string stringIn)
+        {
+            return "<![CDATA['" + stringIn + "']]>";
+        }
+
+        public string alphaNumSpace(string stringIn)
+        {
+            return Regex.Replace(stringIn, "(?<=[0-9])(?=[A-Za-z])|(?<=[A-Za-z])(?=[0-9])", " ");
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            // get a list of nodes
+            //XmlNodeList aNodes = doc.SelectNodes("Tags/Tag");
+
+            // loop through all AID nodes
+            foreach (XmlNode aNode in doc.GetElementsByTagName("Tag"))
+            {
+                // grab "x" attribute
+                XmlAttribute currentDataType = aNode.Attributes["DataType"];
+
+                // check if that attribute even exists...
+                if (currentDataType != null)
+                {
+                    // if yes - read its current value
+                    string currentDataTypeValue = currentDataType.Value;
+
+                    // check if IQ_DO_CONTROL datatype and grab info
+                    if (currentDataTypeValue.Equals("IQ_DO_CONTROL") | currentDataTypeValue.Equals("IQC_DO_CONTROL") | currentDataTypeValue.Equals("IQC_MOTOR_CONTROL") | currentDataTypeValue.Equals("IQC_VALVE_DISCRETE") | currentDataTypeValue.Equals("IQC_VALVE_MODULATION_CONTROL") | currentDataTypeValue.Equals("IQ_VFD_CONTROL") | currentDataTypeValue.Equals("IQ_AI_CONTROL") | currentDataTypeValue.Equals("IQ_FLOW_METER"))
+                    {
+
+                        string tag = aNode.Attributes["Name"].Value;
+                        if (chkSpaceTags.Checked) { tag = alphaNumSpace(tag); }
+
+                        string description = "";
+                        XmlNode descriptionNode = aNode.SelectSingleNode("Description");
+                        if (descriptionNode != null)
+                        {
+                            description = descriptionNode.InnerText;//.Substring(9);
+                        }
+                        string dataType = currentDataTypeValue;
+
+
+                        if (currentDataTypeValue.Equals("IQ_DO_CONTROL") | currentDataTypeValue.Equals("IQC_DO_CONTROL") | currentDataTypeValue.Equals("IQC_VALVE_DISCRETE") | currentDataTypeValue.Equals("IQC_MOTOR_CONTROL") | currentDataTypeValue.Equals("IQC_VALVE_MODULATION_CONTROL"))
+                        {
+                            // check for comments for HOA DINT
+                            XmlNode comments = aNode.SelectSingleNode("Comments");
+                            if(comments != null)
+                            {
+                            XmlNode commentsNew = doc.CreateElement("Comments");
+                            commentsNew.InnerXml = Properties.Resources.TextFile2.ToString().Replace("<![CDATA[", "<![CDATA[" + tag);
+                            aNode.PrependChild(commentsNew);
+                            }  
+
+                            // Move Description Up
+                            aNode.RemoveChild(aNode.SelectSingleNode("Description"));
+                            aNode.PrependChild(descriptionNode);
+                        }
+
+
+                        // write to tag len
+                        try
+                        {
+                            string stringOff = "Off";
+                            string stringOn = "On";
+                            foreach(string csvTag in listBox2.Items)
+                            {
+                                string[] splitted = csvTag.Split('|');
+                                if (splitted[0].Equals(tag)){
+                                    stringOn = splitted[1];
+                                    stringOff = splitted[2];
+                                };
+                            }
+
+                            //aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='Tag']/DataValueMember[@Name='LEN']").Attributes["Value"].Value = tag.Length.ToString();
+                            //aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='Tag']/DataValueMember[@Name='DATA']").InnerXml = cdataWrap(tag);
+                            //aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='Description']/DataValueMember[@Name='LEN']").Attributes["Value"].Value = description.Length.ToString();
+                            //aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='Description']/DataValueMember[@Name='DATA']").InnerXml = cdataWrap(description);
+
+                            aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='WWOnString']/DataValueMember[@Name='LEN']").Attributes["Value"].Value = stringOn.Length.ToString();
+                            aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='WWOnString']/DataValueMember[@Name='DATA']").InnerXml = cdataWrap(stringOn);
+
+                            aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='WWOffString']/DataValueMember[@Name='LEN']").Attributes["Value"].Value = stringOff.Length.ToString();
+                            aNode.SelectSingleNode("Data/Structure/StructureMember[@Name='WWOffString']/DataValueMember[@Name='DATA']").InnerXml = cdataWrap(stringOff);
+                        }
+                        catch (NullReferenceException p)
+                        {
+                            Console.WriteLine(p.Message);
+                        }
+                    }
+                }
+            }
+            foreach (IQ_X_CONTROL x in Array_IQ_X_CONTROL)
+            {
+                Console.WriteLine(x.Name + " | " + x.DescPLC);
+                // adds spaces between alpha and num
+                //listBox1.Items.Add(Regex.Replace(x.Name, "(?<=[0-9])(?=[A-Za-z])|(?<=[A-Za-z])(?=[0-9])"," ") + " | " + x.DataType + " | " + x.DescPLC);
+                listBox1.Items.Add(x.Name + " | " + x.DataType + " | " + x.DescPLC);
+            }
+            // save the XmlDocument back to disk
+            doc.Save(Outfolder + "\\" + "XMLFileOut.xml");
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            doc.Load(pathFile);
+
+            // loop through all AID nodes
+            foreach (XmlNode aNode in doc.GetElementsByTagName("Tag"))
+            {
+                // grab "x" attribute
+                XmlAttribute currentDataType = aNode.Attributes["DataType"];
+
+                // check if that attribute even exists...
+                if (currentDataType != null)
+                {
+                    // if yes - read its current value
+                    string currentValue = currentDataType.Value;
+                    if (!checkedListBox1.Items.Contains(currentValue))
+                    {
+                        checkedListBox1.Items.Add(currentValue);
+                    }
+
+                    // check if IQ_DO_CONTROL datatype and grab info
+                    if (currentValue.Equals("IQ_DO_CONTROL") | currentValue.Equals("IQC_DO_CONTROL") | currentValue.Equals("IQC_VALVE_DISCRETE") | currentValue.Equals("IQC_MOTOR_CONTROL") | currentValue.Equals("IQC_AO_CONTROL") | currentValue.Equals("IQ_DI_CONTROL") | currentValue.Equals("IQ_AO_CONTROL") | currentValue.Equals("IQ_VFD_CONTROL") | currentValue.Equals("IQ_AI_CONTROL") | currentValue.Equals("IQ_FLOW_METER"))
+                    {
+                        //Console.WriteLine(aNode.Attributes["Name"].Value);
+                        //string tag = Regex.Replace(aNode.Attributes["Name"].Value, "(?<=[0-9])(?=[A-Za-z])|(?<=[A-Za-z])(?=[0-9])", " "); // adds spaces between alpha and num
+                        string tag = aNode.Attributes["Name"].Value;
+                        string description = "";
+                        XmlNode descriptionNode = aNode.SelectSingleNode("Description");
+                        if (descriptionNode != null)
+                        {
+                            description = descriptionNode.InnerText;//.Substring(9);
+                        }
+                        string dataType = currentValue;
+                        //description = description.Substring(0, description.Length - 3);
+                        Array_IQ_X_CONTROL.Add(new IQ_X_CONTROL(tag, description, dataType));
+                    }
+                }
+            }
+            foreach (IQ_X_CONTROL x in Array_IQ_X_CONTROL)
+            {
+                Console.WriteLine(x.Name + " | " + x.DescPLC);
+                // adds spaces between alpha and num
+                //listBox1.Items.Add(Regex.Replace(x.Name, "(?<=[0-9])(?=[A-Za-z])|(?<=[A-Za-z])(?=[0-9])"," ") + " | " + x.DataType + " | " + x.DescPLC);
+                listBox1.Items.Add(x.Name + " | " + x.DataType + " | " + x.DescPLC);
+            }
+
+            // read csv to array
+            StreamReader sr = new StreamReader(pathCSV);
+            string line;
+            string[] row = new string[2];
+            while ((line = sr.ReadLine()) != null)
+            {
+                row = line.Split(',');
+
+                // for every csv line, make file
+                string text = "";
+                text = text.Replace("TAG_HOLDER", row[0].Replace("XX", "  "));
+                text = text.Replace("DESC_HOLDER", row[1].Replace(" ", " "));
+                //File.WriteAllText(folder + "\\" + row[0] + ".par", text, Encoding.Unicode);
+                listBox2.Items.Add(row[2].Replace('-', '_') + "|" + row[16] + "|" + row[17]);
+            }
+
+        }
+
+        private void btnHOA_Click(object sender, EventArgs e)
+        {
+            string hoaOut = "";
+            int count = 10;
+            Regex theRegex = new Regex("Rung Number=.?."); // not catching last double quote, . can be 0
+            foreach (IQ_X_CONTROL x in Array_IQ_X_CONTROL)
+            {
+                hoaOut += theRegex.Replace(Properties.Resources.HOA.ToString().Replace("VFD800", x.Name), delegate (Match thisMatch) { return "Rung Number=\"" + count++.ToString(); });
+            }
+            File.WriteAllText(Outfolder + "\\" + "HOAout.txt", hoaOut, Encoding.Unicode);
+        }
+
+        private void btnAlarms_Click(object sender, EventArgs e)
+        {
+            string alarmOut = "";
+            int count = 0;
+            Regex theRegex = new Regex("Rung Number=.?."); // not catching last double quote, . can be 0
+            foreach (IQ_X_CONTROL x in Array_IQ_X_CONTROL)
+            {
+                alarmOut += theRegex.Replace(Properties.Resources.AlarmRungsExport.ToString().Replace("TAG_1", x.Name), delegate (Match thisMatch) { return "Rung Number=\"" + count++.ToString(); });
+            }
+            File.WriteAllText(Outfolder + "\\" + "ALARMSout.txt", alarmOut, Encoding.Unicode);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                pathCSV = file.FileName;
+                textBox2.Text = pathCSV;
+            }
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog file = new FolderBrowserDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                Outfolder = file.SelectedPath;
+                label5.Text = Outfolder;
+            }
+        }
+    }
+}
